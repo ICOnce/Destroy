32ce680f7fb42f172c21e9fd8ac328f9e5a78141
ICOnce <noaellegaard@gmail.com>
2024-02-29 17:04:19 +0100

    Testing amount of verts and tris

--- Assets/Scripts/Calc.cs
+++ Assets/Scripts/Calc.cs
@@ -30,14 +30,22 @@ public class Calc : MonoBehaviour
 
     private void Destruction()
     {
+        vertice1.Clear();
+        vertice2.Clear();
         //Vector3 normal = new Vector3(Random.Range(0, 1), Random.Range(0, 1), Random.Range(0, 1));
         Vector3 normal = new Vector3(0, 1, 0);
-        Debug.Log(target.GetComponent<MeshFilter>().mesh.vertices.Length);
+
+        Debug.Log(target.GetComponent<MeshFilter>().mesh.triangles.Length);
+        foreach (int tri in target.GetComponent<MeshFilter>().mesh.triangles)
+        {
+            Debug.Log(target.GetComponent<MeshFilter>().mesh.vertices[tri]);
+        }
+
+
         for (int i = 0; i < target.GetComponent<MeshFilter>().mesh.vertices.Length; i++)
         {
             Vector3 v = target.GetComponent<MeshFilter>().mesh.vertices[i];
-            det = -normal.x*pop.x - normal.y*pop.y - normal.z*pop.z;
-            Debug.Log(v);
+            //det = -normal.x*pop.x - normal.y*pop.y - normal.z*pop.z;
             sideDecider = normal.x * v.x + normal.y * v.y + normal.z * v.z + det;
             if (sideDecider == 0)
             {
