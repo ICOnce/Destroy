0ebc5ae1ea62ab920754f86efc31a40e17b07f2c
ICOnce <noaellegaard@gmail.com>
2024-03-01 01:18:32 +0100

    Math makes my head hurt

--- Assets/Scripts/Calc.cs
+++ Assets/Scripts/Calc.cs
@@ -11,10 +11,13 @@ public class Calc : MonoBehaviour
     private List<Vector3> vertice1 = new List<Vector3>();
     private List<Vector3> vertice2 = new List<Vector3>();
 
-    private float sideDecider;
-    private float det;
+    private List<int> tris1 = new List<int>();
+    private List<int> tris2 = new List<int>();
 
-    Vector3 pop;
+
+
+    private Vector3 pop;
+    private Vector3 normal;
 
     [SerializeField] private GameObject target;
 
@@ -25,7 +28,7 @@ public class Calc : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) Destruction();
+        if (Input.GetKeyDown(KeyCode.Space)) Debug.Log(Intersect(new Vector3(2, 10, 4), new Vector3(3, -1, 0)));
     }
 
     private void Destruction()
@@ -33,34 +36,57 @@ public class Calc : MonoBehaviour
         vertice1.Clear();
         vertice2.Clear();
         //Vector3 normal = new Vector3(Random.Range(0, 1), Random.Range(0, 1), Random.Range(0, 1));
-        Vector3 normal = new Vector3(0, 1, 0);
+        normal = new Vector3(2, 4, 1);
 
-        Debug.Log(target.GetComponent<MeshFilter>().mesh.triangles.Length);
-        foreach (int tri in target.GetComponent<MeshFilter>().mesh.triangles)
+        for (int i = 0; i < target.GetComponent<MeshFilter>().mesh.triangles.Length; i++)
         {
-            Debug.Log(target.GetComponent<MeshFilter>().mesh.vertices[tri]);
+            Vector3 v1 = target.GetComponent<MeshFilter>().mesh.vertices[target.GetComponent<MeshFilter>().mesh.triangles[i + 0]];
+            Vector3 v2 = target.GetComponent<MeshFilter>().mesh.vertices[target.GetComponent<MeshFilter>().mesh.triangles[i + 1]];
+            Vector3 v3 = target.GetComponent<MeshFilter>().mesh.vertices[target.GetComponent<MeshFilter>().mesh.triangles[i + 2]];
+            if (SideDecider(v1) > 0 && SideDecider(v2) > 0 && SideDecider(v3) > 0)
+            {
+                vertice1.Add(v1);
+                vertice1.Add(v2);
+                vertice1.Add(v3);
+                tris1.Add(i + 0);
+                tris1.Add(i + 1);
+                tris1.Add(i + 2);
+            }
+            else if (SideDecider(v1) > 0 && SideDecider(v2) > 0 && SideDecider(v3) < 0)
+            {
+                vertice1.Add(v1);
+                vertice1.Add(v2);
+                vertice1.Add(Intersect(v3, v1));
+                vertice1.Add(Intersect(v3, v2));
+
+                vertice2.Add(Intersect(v3, v1));
+                vertice2.Add(Intersect(v3, v2));
+                vertice2.Add(v3);
+            }
         }
+    }
 
+    public float SideDecider(Vector3 vertex)
+    {
+        //float det = -normal.x * pop.x - normal.y * pop.y - normal.z * pop.z;
+        return normal.x * vertex.x + normal.y * vertex.y + normal.z * vertex.z /*+ det*/;
+    }
 
-        for (int i = 0; i < target.GetComponent<MeshFilter>().mesh.vertices.Length; i++)
-        {
-            Vector3 v = target.GetComponent<MeshFilter>().mesh.vertices[i];
-            //det = -normal.x*pop.x - normal.y*pop.y - normal.z*pop.z;
-            sideDecider = normal.x * v.x + normal.y * v.y + normal.z * v.z + det;
-            if (sideDecider == 0)
-            {
-                vertice1.Add(v);
-                vertice2.Add(v);
-            }
+    private Vector3 Intersect (Vector3 vert1, Vector3 vert2)
+    {
 
-            if (sideDecider > 0)
-            {
-                vertice1.Add(v);
-            }
-            if (sideDecider < 0)
-            {
-                vertice2.Add(v);
-            }
-        }
+        normal = new Vector3(2, 4, 1);
+
+        float x0 = vert1.x;
+        float y0 = vert1.y;
+        float z0 = vert1.z;
+
+        Vector3 lineR = vert2 - vert1 ;
+        float t = -(normal.x * x0 + normal.y * y0 + normal.z * z0)/(normal.x * lineR.x + normal.y * lineR.y + normal.z * lineR.z);
+        Vector3 intersect = new Vector3(
+            x0 + lineR.x * t,
+            y0 + lineR.y * t,
+            z0 + lineR.z * t);
+        return intersect;
     }
 }
