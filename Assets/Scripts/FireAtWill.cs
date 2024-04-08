d3b55acfe77a55d4e996f806211395fce4ac28b4
ICOnce <noaellegaard@gmail.com>
2024-04-08 07:33:42 +0200

    Made aim accurate

--- Assets/Scripts/FireAtWill.cs
+++ Assets/Scripts/FireAtWill.cs
@@ -7,6 +7,7 @@ public class FireAtWill : MonoBehaviour
     [SerializeField] private GameObject projectile;
     [SerializeField] private float projectileSpeed;
     [SerializeField] private Camera cam;
+    private Vector3 hitPoint;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,11 +17,17 @@ public class FireAtWill : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         if (Input.GetMouseButtonDown(0))
         {
-            GameObject temp = Instantiate(projectile, cam.transform.position, Quaternion.identity);
-            temp.AddComponent<Rigidbody>().AddForce(cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10) - cam.transform.position) * projectileSpeed);
-            temp.GetComponent<Rigidbody>().useGravity = false;
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hitInfo))
+            {
+                hitPoint = hitInfo.point;
+                GameObject temp = Instantiate(projectile, cam.transform.position, Quaternion.identity);
+                temp.AddComponent<Rigidbody>().AddForce((hitPoint - cam.transform.position) * projectileSpeed);
+                temp.GetComponent<Rigidbody>().useGravity = false;
+            }
         }
     }
 }
